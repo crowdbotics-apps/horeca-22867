eb5ae23719ff9cce24f34eed999c55467034d827
Crowdbotics <team@crowdbotics.com>
2020-11-21 18:43:27 +0000

    Update models

--- backend/horeca_22867/wsgi.py
+++ backend/horeca_22867/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'horeca_22867.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "horeca_22867.settings")
 
 application = get_wsgi_application()
